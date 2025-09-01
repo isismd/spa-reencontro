92bb684a1db80650400645cbc3aafb6c17e9e249
Isis Milena Daron <daronisism@gmail.com>
2025-09-01 00:22:22 -0400

    fix: Ajusta toast do sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { RouterProvider } from "react-router-dom";
-import { ThemeProvider } from "./components/layout/ThemeProvider";
+import { ThemeProvider, useTheme } from "./components/layout/ThemeProvider";
 import { router } from "./routes";
 import { Toaster } from "./components/ui/sonner";
 
+function AppContent() {
+  const { theme } = useTheme();
+
+  return (
+    <>
+      <Toaster position="top-right" richColors theme={theme} />
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
 function App() {
   return (
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <Toaster position="top-right" />
-      <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <AppContent />
     </ThemeProvider>
   );
 }
